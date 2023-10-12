4a87fb9c42286d4fe811640b17732991c17c99de
Dorian Thompson <dorianthompson2@Dorians-MBP.fios-router.home>
2023-10-12 07:58:45 -0400

    Add BlogPostForm component and refactor to Create and Edit Screens

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -6,6 +6,10 @@ const blogReducer = (state, action) => {
             return [...state, { id: Math.floor(Math.random() * 99999) ,title: action.payload.title, content: action.payload.content}]
         case 'delete_blogpost':
             return state.filter(blogPost => blogPost.id !== action.payload)
+        case 'edit_blogpost':
+            return state.map(blogPost => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost;
+            })
         default:
             return state;
     }
@@ -14,7 +18,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = dispatch => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: {title, content}});
-        callback();
+        if(callback){
+            callback();
+        }
     }
 }
 
@@ -24,4 +30,13 @@ const deleteBlogPost = dispatch => {
     }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost } , [{ title: "TEST POST", content: "TEST CONTENT", id: 1}]);
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({type: 'edit_blogpost', payload: { id, title, content }})
+        if(callback){
+            callback();
+        }
+    }
+}
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost } , [{ title: "TEST POST", content: "TEST CONTENT", id: 1}]);
